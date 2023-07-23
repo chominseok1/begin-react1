45536a30b11e3a50ffa1e08fd1d52e33f4be1b2e
chominseok1 <mins2713@naver.com>
2023-07-23 17:24:29 +0900

    StartReact!!

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg'; // 사진 불러오기 
+import './App.css'; 
+//         /*{}는 안에 자바스크립트 코드*/
+function App() { // 컴포넌트를 함수로 
+  return (
+    <div className="App">
+     <h1>하이</h1>
+    </div>
+  );
+}
+
+export default App; 
+//이 코드는 App 라는 컴포넌트를 내보내겠다는 의미입니다. 이렇게 해주면 다른 컴포넌트에서 불러와서 사용 할 수 있습니다.
---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // html or jsp 로 치면 <style></style>
+import App from './App'; // com.sist.dao 패키지 불러오기 => 리액트에서는 component라고 한다.
+import reportWebVitals from './reportWebVitals';
+ // 태그들을 감싸려면 위에 큰 태그 안에 넣어줘야함! 태그는 꼭 닫혀있어야한다.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+  <h1>Tl</h1>
+  <App/>
+  </>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
